8408e5efd4705c0c71a8b182cb651f3b9f2568dd
Ben Lieblich <benjamin@Bens-MacBook-Pro.local>
2015-06-15 23:24:11 -0400

    Added Facebook login

--- Vane/ViewController.swift
+++ Vane/ViewController.swift
@@ -7,12 +7,41 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if FBSDKAccessToken.currentAccessToken() == nil {
+            println("Not logged in")
+        } else {
+            println("logged in")
+        }
+        
+        var loginButton = FBSDKLoginButton()
+        loginButton.readPermissions = ["public_profile","email","user_friends"]
+        loginButton.center = self.view.center
+        loginButton.delegate = self
+        self.view.addSubview(loginButton)
+    }
+    
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        if error == nil {
+            println("login complete")
+            self.performSegueWithIdentifier("LoggedInSegue", sender: self)
+        } else {
+            println("error: \(error.localizedDescription)")
+        }
+        
+        
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        println("Logged Out!")
     }
 
     override func didReceiveMemoryWarning() {
